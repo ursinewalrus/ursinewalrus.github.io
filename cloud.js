8376157e70e7f4b93897fd9b221791717a24bf15
Josh Kerxhalli-Kleinfield <joshkerxhallik@gmail.com>
2018-04-06 16:06:54 -0500

    gumshoe ToC initial

---
+++ cloud.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+
+	firebase.initializeApp({
+	  apiKey: "AIzaSyA2QkD76en09SQFhR8ey4MD9qU9Qu2Gk60",                            
+	  authDomain: "test-game-374da.firebaseapp.com",         
+	});
+
+	var uiConfig = {
+        signInSuccessUrl: 'ursinewalrus.github.io',
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        ],
+        // Terms of service url.
+        // tosUrl: '<your-tos-url>'
+      };
+
+      // Initialize the FirebaseUI Widget using Firebase.
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      // The start method will wait until the DOM is loaded.
+     // if (ui.isPendingRedirect()) {
+		  ui.start('#firebaseui-auth-container', uiConfig);
+	 // }
+
+});
